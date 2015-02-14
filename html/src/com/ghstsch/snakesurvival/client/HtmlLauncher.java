16d889b67cf119bdc8b0d64cbe3245f2c249c720
GhostSeech <ghstsch@yandex.ru>
2015-02-14 15:38:07 +0300

    Initial commit

---
+++ html/src/com/ghstsch/snakesurvival/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.ghstsch.snakesurvival.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.ghstsch.snakesurvival.SnakeSurvival;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new SnakeSurvival();
+        }
+}
