5cf2fb9b18e4e6348bc7bde1fe563c153c486004
GhostSeech <ghstsch@yandex.ru>
2015-02-15 11:20:38 +0300

    Added Screen and Input

---
+++ core/src/com/ghstsch/snakesurvival/InputHandler.java
@@ -0,0 +1,60 @@
+package com.ghstsch.snakesurvival;
+
+/**
+ * Created by aaaa on 15.02.2015.
+ */
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+
+public class InputHandler {
+    private static final int MAX_BUTTONS = 6;
+
+    public static final int W = 0;
+    public static final int S = 1;
+    public static final int A = 2;
+    public static final int D = 3;
+    public static final int SPACE = 4;
+    public static final int MOUSE = 5;
+
+    private static int mousePosX;
+    private static int mousePosY;
+
+    static boolean keys[];
+    static boolean prevKeys[];
+    static {
+        keys = new boolean[MAX_BUTTONS];
+        prevKeys = new boolean[MAX_BUTTONS];
+    }
+
+    static void update() {
+        for(int i = 0; i < MAX_BUTTONS; i++) prevKeys[i] = keys[i];
+
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) keys[A] = true;
+        else keys[A] = false;
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) keys[D] = true;
+        else keys[D] = false;
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) keys[W] = true;
+        else keys[W] = false;
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) keys[S] = true;
+        else keys[S] = false;
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) keys[SPACE] = true;
+        else keys[SPACE] = false;
+        if (Gdx.input.isTouched()) keys[MOUSE] = true;
+        else keys[MOUSE] = false;
+    }
+
+    static boolean isKeyPressed(int key) {
+        if(key > MAX_BUTTONS) return false;
+        return keys[key] && prevKeys[key];
+    }
+    static boolean isKeyDown(int key) {
+        if(key > MAX_BUTTONS) return false;
+        return keys[key];
+    }
+    static int getMouseX() {
+        return mousePosX;
+    }
+    static int getMousePosY() {
+        return mousePosY;
+    }
+}
--- core/src/com/ghstsch/snakesurvival/Screen.java
+++ core/src/com/ghstsch/snakesurvival/Screen.java
@@ -3,8 +3,9 @@ package com.ghstsch.snakesurvival;
 /**
  * Created by aaaa on 14.02.2015.
  */
-public abstract class Screen {
-    public abstract void draw();
-    public abstract void update();
-    public abstract void handleInput();
+public interface Screen {
+    public void init();
+    public void draw();
+    public void update(float dt);
+    public void handleInput();
 }
